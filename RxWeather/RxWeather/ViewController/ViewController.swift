08cd1fcf24799382aa1ce6b4834a3163e1976dc5
Hoon94 <dleognsdl1@naver.com>
2024-12-09 23:20:48 +0900

    feat: RxWeather 프로젝트 생성 및 라이브러리 의존성 추가

---
+++ RxWeather/RxWeather/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxWeather
+//
+//  Created by Daehoon Lee on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
