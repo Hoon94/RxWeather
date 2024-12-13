ea815ba40457e0c48937e2c40c85ad2f75bae8a9
Hoon94 <dleognsdl1@naver.com>
2024-12-13 23:58:30 +0900

    feat: 위치 서비스 사용에 필요한 Delegate Proxy 구현

---
+++ RxWeather/RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
@@ -0,0 +1,54 @@
+//
+//  RxCLLocationManagerDelegateProxy.swift
+//  RxWeather
+//
+//  Created by Daehoon Lee on 12/13/24.
+//
+
+import CoreLocation
+import Foundation
+import RxCocoa
+import RxSwift
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+public class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    
+    // MARK: - Static
+    
+    public static func registerKnownImplementations() {
+        self.register { RxCLLocationManagerDelegateProxy(locationManager: $0) }
+    }
+    
+    // MARK: - Lifecycle
+    
+    public init(locationManager: CLLocationManager) {
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+}
+
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    public var didUpdateLocation: Observable<[CLLocation]> {
+        let selector = #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:))
+        
+        return delegate.methodInvoked(selector)
+            .map { parameters in
+                return parameters[1] as? [CLLocation] ?? []
+            }
+    }
+    
+    public var didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> {
+        let selector = #selector(CLLocationManagerDelegate.locationManagerDidChangeAuthorization(_:))
+        
+        return delegate.methodInvoked(selector)
+            .map { parameters in
+                return CLAuthorizationStatus(rawValue: parameters[1] as? Int32 ?? 0) ?? .notDetermined
+            }
+    }
+}
